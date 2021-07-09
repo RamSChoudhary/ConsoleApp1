cf780f6221a7f8c61a647709665d7650772e0304
RamSChoudhary <ramrit10@gmail.com>
2021-07-09 15:01:34 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
-using System.Linq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+//using System.Linq;
 
 namespace ConsoleApp1
 {
@@ -7,7 +10,14 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Check());
+            //Console.WriteLine(Check());
+            //DuplicateStrings();
+            //UniqueStrings();
+            //ReverseString();
+            //ReverseWords();
+            //WordCounInSentence();
+            //PallindromeCheck();
+            CheckMaxWordCount();
         }
 
         private static string Check()
@@ -26,7 +36,7 @@ namespace ConsoleApp1
             {
                 result += palin[y].ToString();
             }
-            if(result == input)
+            if (result == input)
             {
                 result = "$" + result + "$";
                 return result;
@@ -44,5 +54,188 @@ namespace ConsoleApp1
 
             return result;
         }
+
+        private static string DuplicateStrings()
+        {
+            string input = "Ramsharan Choudhary";
+
+            StringBuilder result = new StringBuilder();
+
+            int parentindex = 0, childindex = 0;
+            foreach (char a in input.ToLower())
+            {
+                foreach (char b in input.ToLower())
+                {
+                    if (a == b & parentindex != childindex)
+                    {
+                        if (!result.ToString().Contains(b.ToString()))
+                            result.Append(b.ToString() + ", ");
+                    }
+                    childindex++;
+                }
+                parentindex++;
+                childindex = 0;
+            }
+            return result.ToString();
+
+            //StringBuilder result = new StringBuilder();
+            //StringBuilder duplicateChar = new StringBuilder();
+
+            //foreach (var item in input)
+            //{
+            //    if (result.ToString().IndexOf(item.ToString().ToLower()) == -1)
+            //    {
+            //        result.Append(item);
+            //    }
+            //    else
+            //    {
+            //        duplicateChar.Append(item);
+            //    }
+            //}
+
+            //return duplicateChar.ToString();
+
+            //
+        }
+
+        private static string UniqueStrings()
+        {
+            string input = "Ramsharan Choudhary";
+
+            StringBuilder result = new StringBuilder();
+
+            int parentindex = 0;
+            foreach (char a in input.ToLower())
+            {
+                foreach (char b in input.ToLower())
+                {
+                    if (a == b)
+                    {
+                        parentindex++;
+                    }
+
+                }
+                if (parentindex == 1)
+                {
+                    if (!result.ToString().Contains(a.ToString()))
+                    {
+                        result.Append(a.ToString() + ", ");
+                    }
+                }
+                parentindex = 0;
+            }
+            return result.ToString();
+        }
+
+        private static string ReverseString()
+        {
+            string input = "Ramsharan Choudhary";
+
+            StringBuilder result = new StringBuilder();
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                result.Append(input[i]);
+            }
+
+            return result.ToString();
+        }
+
+        private static string ReverseWords()
+        {
+            string input = "Ramsharan Choudhary";
+
+            string[] separator = input.Split(' ');
+            StringBuilder result = new StringBuilder();
+            for (int z = 0; z <= separator.Length - 1; z++)
+            {
+                for (int i = separator[z].Length - 1; i >= 0; i--)
+                {
+                    result.Append(separator[z][i]);
+                }
+                result.Append(" ");
+            }
+
+            return result.ToString();
+        }
+
+        private static int WordCounInSentence()
+        {
+            string input = "Ramsharan Choudhary is a genious person. He did BE, and with first Class.";
+
+            string[] separator = input.Split(' ', ',', '.');
+            ArrayList arrayList = new ArrayList();
+
+            for (int i = 0; i <= separator.Length - 1; i++)
+            {
+                if (!separator[i].ToString().Equals(string.Empty))
+                {
+                    arrayList.Add(separator[i]);
+                }
+            }
+
+            return arrayList.Count;
+        }
+
+        private static string PallindromeCheck()
+        {
+            string input = "radar", result = string.Empty;
+            string[] split = input.Split(' ');
+
+            for (int y = input.Length - 1; y >= 0; y--)
+            {
+                result += input[y].ToString();
+            }
+            if (result == input)
+            {
+                return result;
+
+            }
+            else
+            {
+                result = "Not Palindrome";
+            }
+
+            return result;
+        }
+
+        private static string CheckMaxWordCount()
+        {
+            string input = "radar is radar nothing else and radar is very good", result = string.Empty;
+
+
+            string[] separator = input.Split(' ', ',', '.');
+            Dictionary<string, int> arrayList = new Dictionary<string, int>();
+
+            int parentindex = 0, childindex = 0;
+            for (int i = 0; i <= separator.Length - 1; i++)
+            {
+                if (!arrayList.ContainsKey(separator[i]))
+                    for (int z = 0; z <= separator.Length - 1; z++)
+                    {
+
+                        if (separator[i].ToString().Equals(separator[z].ToString()) && parentindex != childindex)
+                        {
+                            if (arrayList.ContainsKey(separator[i]))
+                            {
+                                int count = arrayList[separator[i]];
+                                arrayList[separator[i]] = count + 1;
+                            }
+                        }
+                        else if (!arrayList.ContainsKey(separator[i]))
+                        {
+                            arrayList.Add(separator[i], 1);
+                        }
+                        childindex++;
+                    }
+                parentindex++;
+                childindex = 0;
+            }
+
+
+            return result;
+        }
     }
+
+
 }
+
